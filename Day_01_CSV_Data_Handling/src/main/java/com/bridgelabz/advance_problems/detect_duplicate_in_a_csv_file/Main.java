6b8ac0846cf75451ea3984c319815c8821defaf7
Manish <patelmanish131149198@gmail.com>
2025-02-18 23:36:38 +0530

    [Manish]Add. Program for detecting duplicate in a csv file

---
+++ Day_01_CSV_Data_Handling/src/main/java/com/bridgelabz/advance_problems/detect_duplicate_in_a_csv_file/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.advance_problems.detect_duplicate_in_a_csv_file;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "src/main/resources/duplicate_student.csv";
+        DetectDuplicates.findDuplicates(filePath);
+    }
+}

431532859cbdd1254bb953d9825481659002956a
Manish <patelmanish131149198@gmail.com>
2025-02-18 23:38:42 +0530

    [Manish]Add. Program for validating csv data before processing

---
+++ Day_01_CSV_Data_Handling/src/main/java/com/bridgelabz/advance_problems/validate_csv_data_before_processing/Main.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.advance_problems.validate_csv_data_before_processing;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath = "src/main/resources/validate_employee.csv";
+        ValidateCSV.validateCSV(filePath);
+    }
+}

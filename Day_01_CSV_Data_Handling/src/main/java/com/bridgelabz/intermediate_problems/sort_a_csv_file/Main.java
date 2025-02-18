08ba3c128b4db064aef8d5af061dc9d77709fa98
Manish <patelmanish131149198@gmail.com>
2025-02-18 23:15:33 +0530

    [Manish]Add. Program for sort a csv file

---
+++ Day_01_CSV_Data_Handling/src/main/java/com/bridgelabz/intermediate_problems/sort_a_csv_file/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.intermediate_problems.sort_a_csv_file;
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath="src/main/resources/employee.csv";
+        SortCSV.sortBySalary(filePath);
+    }
+}

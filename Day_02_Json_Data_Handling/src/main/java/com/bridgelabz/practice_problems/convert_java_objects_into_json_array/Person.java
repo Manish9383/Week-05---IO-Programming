7f67d8741b668bd86c62b65033373f5f7cfb8b46
Manish <patelmanish131149198@gmail.com>
2025-02-19 23:07:07 +0530

    [Manish]Add. Program for converting java objects into json array

---
+++ Day_02_Json_Data_Handling/src/main/java/com/bridgelabz/practice_problems/convert_java_objects_into_json_array/Person.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.practice_problems.convert_java_objects_into_json_array;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
